66f44f712ba639bfc76dd38a2919374a15dcf5b4
Vidit Prajapati <vidit.msit@gmail.com>
2024-06-23 00:14:22 +0530

    image uploading functionality completed

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ export default function App(){
     <BrowserRouter>
       <Header/>
       
-      <div className="absolute inset-0 -z-10 h-dvh w-full bg-lime-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-lime-400 opacity-20 blur-[100px]">
+      <div className="absolute inset-0 -z-10 bg-div w-full bg-lime-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-lime-400 opacity-20 blur-[100px]">
         
         </div></div>
-      <Routes >
+      <Routes>
         
         <Route path='/' element={<Home/>}/>
         <Route path='/sign-in' element={<SignIn/>}/>
--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,74 @@
-import React from 'react'
-
+import { getDownloadURL, getStorage, uploadBytes, uploadBytesResumable } from 'firebase/storage';
+import React from 'react';
+import { app } from "../firebase";
+import { useState } from 'react';
+import { ref } from 'firebase/storage';
+import { setupListeners } from '@reduxjs/toolkit/query';
 const CreateListing = () => {
+    const [files, setFiles] = useState({});
+    const [formData, setFormData] = useState({
+        imageUrls: [],
+
+    });
+    const [imageUploadError, setImageUploadError] = useState(false);
+    const  [uploading, setUploading] = useState(false);
+    console.log(formData)
+    const handleImageSubmit = (e) =>{
+        if(files.length > 0 && files.length +formData.imageUrls.length<7){
+            console.log("call")
+            setUploading(true);
+            setImageUploadError(false)
+            const promises = [];
+            for (let i = 0; i < files.length; i++) {
+                promises.push(storeImage(files[i]))
+
+            }
+            
+                Promise.all(promises).then((urls) => {
+                    setFormData({...formData , imageUrls: formData.imageUrls.concat(urls)});
+                    setImageUploadError(false);setUploading(false)
+                }).catch((e) => {
+                    setImageUploadError('Max Size of Image can be 2 mb')
+                    console.log(e);
+                    setUploading(false);
+                });
+                
+        }
+        else{
+            setImageUploadError('you can only upload min 1 and max 6 images of the property');
+           setUploading(false)
+        }
+    }
+    const storeImage = async(file) =>{
+        return new Promise ((resolve,reject) => {
+            const storage = getStorage(app);
+                const fileName = file.name + new Date().getTime();
+                const storageRef = ref(storage, fileName);
+                const uploadTask = uploadBytesResumable(storageRef,file);
+                uploadTask.on(
+                    "state_changed",
+                    (snapshot) =>{
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100 ;
+                        console.log(`Upload is ${progress}% done`);
+                    },
+                    (error)=>{
+                        reject(error)
+                    },
+                    () => {
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>{
+                            resolve(downloadURL)
+                        })
+                    }
+                )
+
+        })
+    }
+    const handleDelete =(index) =>{
+        setFormData({
+            ...formData,
+            imageUrls : formData.imageUrls.filter((_,i) => i !== index),
+        })
+    }
   return (
     <main className='p-3 max-w-4xl mx-auto '>
         <h1 className='text-3xl font-bold text-center my-7 text-lime-800'>Create Listing</h1>
@@ -68,9 +136,19 @@ const CreateListing = () => {
                     <span className='font-normal ml-2'>The First Image will be the cover (max 6)</span>
                 </p>
                 <div className='flex gap-4 mt-2'>
-                    <input type="file" id='images' accept='image/*' className='p-3 border rounded-lg w-full border-lime-800' multiple/>
-                    <button className='border-0  bg-green-700 text-white rounded-lg p-4  uppercase hover:shadow-lg hover:bg-green-600 backdrop-blur-[10px]'>Upload</button>
+                    <input onChange={(e)=> setFiles(e.target.files)} type="file" id='images' accept='image/*' className='p-3 border rounded-lg w-full border-lime-800' multiple/>
+                    <button type='button'
+                    onClick={handleImageSubmit} className='border-0  bg-green-700 text-white rounded-lg p-4  uppercase hover:shadow-lg hover:bg-green-600 backdrop-blur-[10px]'>{uploading ? 'Uploading' : 'Upload'}</button>
                 </div>
+            <p className='text-red-700'>{imageUploadError ? imageUploadError :""}</p>
+            {formData.imageUrls.length > 0 && formData.imageUrls.map((url,index) =>(
+                <div key={url} className=' flex justify-between p-3 border-2 border-gray-400 rounded-lg'>
+                    <img src={url} alt="listing image" className='h-10 w-20 object-contain rounded-lg'></img>
+
+                    <button type='button' onClick={()=> handleDelete(index)} className='p-3 bg-red-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Delete</button>
+                </div>
+
+            ))}
             <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create Listing</button>
             </div>
         </form>
