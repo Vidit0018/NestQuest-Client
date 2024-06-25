2f31c496830f4bcca3a9de0a57806ecb663bb78c
Vidit Prajapati <vidit.msit@gmail.com>
2024-06-25 23:15:38 +0530

    search Term added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link ,useNavigate} from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const Header = () => {
+  const navigate = useNavigate(); 
   const { currentUser } = useSelector((state) => state.user);
-  if(currentUser){
-
-    console.log(currentUser.avatar);
+  const [searchTerm,setSearchTerm] = useState('');
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('searchTerm',searchTerm);
+    const searchQuery= urlParams.toString();
+    navigate(`/search?${searchQuery}`)
+    
   }
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(window.location.search)
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl)
+    }
+  },[location.search])
   return (
     <header className="bg-yellow-300 shadow-md ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-2 sm:p-3 gap-3">
@@ -19,15 +33,20 @@ const Header = () => {
           </h1>
         </Link>
         <form
-          action=""
+          onSubmit={handleSubmit}
           className="bg-yellow-100 p-3 rounded-lg flex items-center"
         >
           <input
             type="text"
             placeholder="Search..."
+            value={searchTerm}
+            onChange={(e)=> setSearchTerm(e.target.value)}
             className="bg-transparent focus:outline-none w-20 sm:w-64"
           />
+          <button>
+
           <FaSearch className="text-lime-800" />
+          </button>
         </form>
         <ul className="flex justify-center items-center gap-4 font-3xl font-semibold">
           <Link to="/">
